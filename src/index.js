8f368028bdbb6b2951d748386d9f73aa1e004c60
buliangc <1610696542@qq.com>
2021-06-02 09:51:58 +0800

    tree shaking

--- src/index.js
+++ src/index.js
@@ -55,11 +55,32 @@ arr.map( (item, index) => {
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { count:0 }
+    }
+    incrementCount() {
+        // 注意：这样 *不会* 像预期的那样工作。
+        this.setState({count: this.state.count + 1});
+      }
+      
+    handleSomething() {
+        // 假设 `this.state.count` 从 0 开始。
+        this.incrementCount();
+        this.incrementCount();
+        this.incrementCount();
+        // 当 React 重新渲染该组件时，`this.state.count` 会变为 1，而不是你期望的 3。
+      
+        // 这是因为上面的 `incrementCount()` 函数是从 `this.state.count` 中读取数据的，
+        // 但是 React 不会更新 `this.state.count`，直到该组件被重新渲染。
+        // 所以最终 `incrementCount()` 每次读取 `this.state.count` 的值都是 0，并将它设为 1。
+        console.log(this.state.count);
+        // 问题的修复参见下面的说明。
     }
     render() { 
         return ( 
+            <>
             <h3>这是一个React组件</h3>
+            <button onClick={() => {this.handleSomething()}}>按钮</button>
+            </>
          );
     }
 }
